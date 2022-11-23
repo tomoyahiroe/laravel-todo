ba45d66e9faf21182569c214e1a1e350bdda5a19
tomoyahiroe <tomoya4creative@gmail.com>
2022-11-23 19:37:12 +0900

    feat: migration file

---
+++ database/migrations/2022_11_23_165230_create_todos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('todo', 100);
+            $table->text('description')->nullable();
+            $table->date('due')->nullable();
+            $table->datetime('created_at')->useCurrent()->nullable();
+            $table->datetime('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
