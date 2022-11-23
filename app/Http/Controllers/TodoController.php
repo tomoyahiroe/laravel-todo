d5961042a1720a132288c7e15ad5e0dd5937947f
tomoyahiroe <tomoya4creative@gmail.com>
2022-11-23 21:06:35 +0900

    feat: routing to index

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $allTodo = Todo::all();
+        // dd($allTodo);
+        return view('index', ['allTodo' => $allTodo]);
+    }
+
+    public function create(Request $request)
+    {
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/', [TodoController::class, 'index']);
+
+Route::post('/create', [TodoController::class, 'create']);
