fafeda581ab77657a8917cf47fd99fc85e7196dd
tomoyahiroe <tomoya4creative@gmail.com>
2022-11-30 03:06:46 +0900

    style: fix design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,50 +1,49 @@
 @extends('layouts.default')
 
-<style>
-
-</style>
 
 @section('title', 'Laravel Todo App')
 
 @section('content')
 <div class="todo-app_wrapper">
   @if(isSet($input))
-  <form action="/update" method="POST">
-    <table>
+  <h3>Edit Todo</h3>
+  <form class="todo-form" action="/update" method="POST">
+    <table class="todo-form__table">
       @csrf
       <input type="hidden" name="id" value="{{$input->id}}" />
       <tr>
-        <th>todo</th>
+        <th>Todo</th>
         <td><input type="text" name="todo" value="{{$input->todo}}" required></td>
       </tr>
       <tr>
-        <th>description</th>
-        <td><input type="text" name="description" value="{{$input->description}}"></td>
+        <th>Description</th>
+        <td><textarea name="description" cols="100" rows="5">{{$input->description}}</textarea></td>
       </tr>
       <tr>
-        <th>due</th>
+        <th>Due</th>
         <td><input type="date" name="due" value="{{$input->due}}"></td>
     </table>
-    <input type="submit" />
+    <input type="submit" value="Submit" />
   </form>
 
   @else
-  <form action="/create" method="POST">
-    <table>
+  <h3>Create Todo</h3>
+  <form class="todo-form" action="/create" method="POST">
+    <table class="todo-form__table">
       @csrf
       <tr>
-        <th>todo</th>
+        <th>Todo</th>
         <td><input type="text" name="todo" required></td>
       </tr>
       <tr>
-        <th>description</th>
-        <td><input type="text" name="description"></td>
+        <th>Description</th>
+        <td><textarea name="description" cols="100" rows="5"></textarea></td>
       </tr>
       <tr>
-        <th>due</th>
+        <th>Due</th>
         <td><input type="date" name="due"></td>
     </table>
-    <input type="submit" />
+    <input type="submit" value="Submit" />
   </form>
 
   @endif
@@ -53,20 +52,81 @@
   <div class="todo-list__container">
     <h3>Todo List</h3>
     @if(isSet($allTodo))
-    <hr />
     <table class="todo-table">
       @foreach($allTodo as $todo)
       <tr class="todo-table__row">
         <td class="todo-name">{{$todo->todo}}</td>
-        <td>: {{$todo->description}}</td>
-        <td>due: {{$todo->due}}</td>
-        <td><a href="/update?id={{$todo->id}}">edit</a></td>
-        <td><a href="/delete?id={{$todo->id}}">done!</a></td>
+        <td class="todo-description">: {{$todo->description}}</td>
+        <td>Due: {{$todo->due}}</td>
+        <td>
+          <a class="todo-edit__button" href="/update?id={{$todo->id}}">
+            {!! file_get_contents(public_path('icons/edit_icon.svg')) !!}
+          </a>
+        </td>
+        <td>
+          <a class="todo-delete__button" href="/delete?id={{$todo->id}}">
+            {!! file_get_contents(public_path('icons/done_icon.svg')) !!}
+          </a>
+        </td>
       </tr>
       @endforeach
     </table>
-    <hr />
     @endif
   </div>
 </div>
+
+<style>
+  .todo-form {
+    background-color: #c0c0c0;
+    margin-bottom: 48px;
+    padding: 32px;
+    max-width: 1024px;
+    box-sizing: border-box;
+    color: #1d2630;
+  }
+
+  input,
+  textarea {
+    background-color: #f3f3f3;
+  }
+
+  .todo-form__table {
+    border-spacing: 8px;
+  }
+
+  .todo-form th {
+    text-align: left;
+  }
+
+  .todo-table {
+    width: 100%;
+    max-width: 1024px;
+    border-spacing: 48px;
+    background-color: #c0c0c0;
+    color: #1d2630;
+  }
+
+  .todo-name {
+    font-weight: bold;
+  }
+
+  .todo-edit__button svg {
+    width: 32px;
+    height: 32px;
+    fill: #1d2630;
+    transition: 1s;
+  }
+
+  .todo-delete__button svg {
+    width: 32px;
+    height: 32px;
+    fill: #1d2630;
+    transition: 1s;
+  }
+
+  .todo-edit__button svg:hover {}
+
+  .todo-delete__button:hover {}
+</style>
+
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@yeild('titile')</title>
+  <title>Todo App</title>
 </head>
 
 <body>
@@ -16,22 +16,33 @@
 </body>
 
 <style>
-body {
-  font-size: 16px;
-  width: 100vw;
-  min-height: 100vh;
-  background-color: #1d2630;
-  color: #f3f3f3;
-}
+  body {
+    font-size: 16px;
+    width: 100vw;
+    min-height: 100vh;
+    background-color: #1d2630;
+    color: #f3f3f3;
+    box-sizing: border-box;
+  }
 
-h1 {
-  margin-top: 20px;
-  margin-left: 20px;
-}
+  h1 {
+    margin-top: 20px;
+    margin-left: 60px;
+  }
 
-.component-body {
-  margin: 20px;
-}
+  a {
+    text-decoration: none;
+    color: #f3f3f3;
+  }
+
+  /* a:hover {
+    background-color: #f3f3f3;
+    color: #1d2630;
+  } */
+
+  .component-body {
+    margin: 60px;
+  }
 </style>
 
 </html>
