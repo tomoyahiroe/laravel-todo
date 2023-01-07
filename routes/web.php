14300c7ceac7a2deb2a7e81250b51d7690f8ec25
tomoyahiroe <tomoya4creative@gmail.com>
2023-01-07 20:32:16 +0900

    fix: routing

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::group(['prefix' => 'todo', 'middleware' => 'auth'], function() {
+    Route::get('/', [TodoController::class, 'index']);
+    Route::post('/create', [TodoController::class, 'create']);
+    Route::post('/update', [TodoController::class, 'update']);
+    Route::post('/delete', [TodoController::class, 'delete']);
+});
